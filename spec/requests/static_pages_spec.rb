9116004df98597ffd9cc6273749353e1d3fd623e
Andrew Smith <andrewchristophersmith@gmail.com>
2013-08-11 17:02:31 -0700

    Refactored

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -14,7 +14,11 @@ describe "StaticPages" do
     end
     
     it "should have the right title" do
-      expect(page).to have_title("#{base_title} | Home")
+      expect(page).to have_title("#{base_title}")
+    end
+    
+    it "should not have a custom title" do
+      expect(page).not_to have_title('| Home')
     end
   end
   
